b77d51cfdc87cd37b2794282cece9637e714907a
曹书润 <a936381813@163.com>
2017-07-02 16:24:08 +0800

    init

---
+++ Custom Template/Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,134 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+//
+//                                  _oo8oo_
+//                                 o8888888o
+//                                 88" . "88
+//                                 (| -_- |)
+//                                 0\  =  /0
+//                               ___/'==='\___
+//                             .' \\|     |// '.
+//                            / \\|||  :  |||// \
+//                           / _||||| -:- |||||_ \
+//                          |   | \\\  -  /// |   |
+//                          | \_|  ''\---/''  |_/ |
+//                          \  .-\__  '-'  __/-.  /
+//                        ___'. .'  /--.--\  '. .'___
+//                     ."" '<  '.___\_<|>_/___.'  >' "".
+//                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+//                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+//                =====`-.____`.___ \_____/ ___.`____.-`=====
+//                                  `=---=`
+//
+//
+//               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//                          佛祖保佑         永无bug
+//------------------------------------------------------------------------
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@property (nonatomic,strong,readonly)   UITableView *tableView;
+//@property (nonatomic,strong)   <#type#>             <#name#>
+//@property (nonatomic,copy)     <#type#>             <#name#>
+//@property (nonatomic,assign)   <#type#>             <#name#>
+//@property (nonatomic,weak)     <#type#>             <#name#>
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - life cycle -
+
+- (void)dealloc
+{
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self initData];
+    [self initUI];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+#pragma mark - init -
+
+- (void)initData
+{
+    
+}
+
+- (void)initUI
+{
+    
+}
+
+#pragma mark - tableViewDatasource -
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return <#1#>;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return <#rowHeight#>;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return <#sectionHeight#>;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    static NSString *const cellIdentifier = @"___FILEBASENAME___TableViewCellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+    }
+    
+    return cell;
+}
+
+#pragma mark - tableViewDelegate -
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+#pragma mark - actionSheet -
+
+#pragma mark - private -
+
+
+@end
