da6e41b24d9166c133f13cab9d298c78a0c6d6c6
曹书润 <a936381813@163.com>
2017-09-22 15:09:15 +0800

    增加IS Temple

---
+++ IS Template/Cocoa Touch Class.xctemplate/NSObjectObjective-C/___FILEBASENAME___.h
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+//@class ___FILEBASENAMEASIDENTIFIER___;
+//@protocol ___FILEBASENAMEASIDENTIFIER___Delegate <NSObject>
+//
+//@end
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+//@property (nonatomic, weak)   id<___FILEBASENAMEASIDENTIFIER___Delegate>                  delegate;
+
+@end
---
+++ IS Template/Cocoa Touch Class.xctemplate/UIViewObjective-C/___FILEBASENAME___.mm
@@ -0,0 +1,67 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+//
+//
+//                                  _oo8oo_
+//                                 o8888888o
+//                                 88" . "88
+//                                 (| -_- |)
+//                                 0\  =  /0
+//                               ___/'==='\___
+//                             .' \\|     |// '.
+//                            / \\|||  :  |||// \
+//                           / _||||| -:- |||||_ \
+//                          |   | \\\  -  /// |   |
+//                          | \_|  ''\---/''  |_/ |
+//                          \  .-\__  '-'  __/-.  /
+//                        ___'. .'  /--.--\  '. .'___
+//                     ."" '<  '.___\_<|>_/___.'  >' "".
+//                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+//                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+//                =====`-.____`.___ \_____/ ___.`____.-`=====
+//                                  `=---=`
+//
+//
+//               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//                          佛祖保佑         永无bug
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAME___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self initUI];
+    }
+    return self;
+}
+
+- (void)initUI
+{
+    
+}
+#pragma mark - public Method -
+
+#pragma mark - private Method -
+
+@end
